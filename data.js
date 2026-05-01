68a38d9e28aa37b8328b90451b40ece911eaf24d
vuongskysurferstorage <vuongskysurferstorage1@gmail.com>
2026-04-30 21:50:47 -0400

    upload

---
+++ data.js
@@ -0,0 +1,274 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-1-c-cha-gisu-chu-php-ra-ti-sng-gioan",
+      "name": "1. Крещение Иисуса В Иордане",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.6649957818216627,
+          "pitch": 0.08677225450899329,
+          "rotation": 0,
+          "target": "1-2-c-cha-gisu-d-tic-ci-cana"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.6319923911907424,
+          "pitch": -0.17290606185455815,
+          "title": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 115%;\">Крещение Иисуса В Иордане</span><o:p></o:p></p>",
+          "text": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">Тогда приходит Иисус из Галилеи на Иордан к Иоанну креститься от него. Иоанн же удерживал Его и говорил: мне надобно креститься от Тебя, и Ты ли приходишь ко мне? Но Иисус сказал ему в ответ: оставь теперь, ибо так надлежит нам исполнить всякую правду. Тогда [Иоанн] допускает Его.</div></span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">И, крестившись, Иисус тотчас вышел из воды, – и се, отверзлись Ему небеса, и увидел [Иоанн] Духа Божия, Который сходил, как голубь, и ниспускался на Него. И се, глас с небес глаголющий: Сей есть Сын Мой возлюбленный, в Котором Мое благоволение.<o:p></o:p></span></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">(Мф 3:13–17)<o:p></o:p></p></div>"
+        }
+      ]
+    },
+    {
+      "id": "1-2-c-cha-gisu-d-tic-ci-cana",
+      "name": "2. Откровение Господа Иисуса О Себе Самом На Брачном Пиру В Кане",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.20790684707299256,
+          "pitch": -0.008690813679965714,
+          "rotation": 0,
+          "target": "2-3-c-cha-gisu-rao-ging-nc-tri-v-ku-gi-sm-hi"
+        },
+        {
+          "yaw": 1.3359429840287653,
+          "pitch": 0.13937177954095148,
+          "rotation": 0,
+          "target": "0-1-c-cha-gisu-chu-php-ra-ti-sng-gioan"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.24373764674771792,
+          "pitch": -0.2789593316817669,
+          "title": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 115%;\">Откровение Господа Иисуса О Себе Самом На Брачном Пиру В Кане</span><o:p></o:p></p>",
+          "text": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">На третий день был брак в Кане Галилейской, и Матерь Иисуса была там. Был также зван Иисус и ученики Его на брак. И как недоставало вина, то Матерь Иисуса говорит Ему: вина нет у них. Иисус говорит Ей: что Мне и Тебе, Жено? еще не пришел час Мой. Матерь Его сказала служителям: что скажет Он вам, то сделайте.</div></span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">Было же тут шесть каменных водоносов, стоявших [по обычаю] очищения Иудейского, вмещавших по две или по три меры. Иисус говорит им: наполните сосуды водою. И наполнили их до верха. И говорит им: теперь почерпните и несите к распорядителю пира. И понесли. Когда же распорядитель отведал воды, сделавшейся вином, – а он не знал, откуда [это вино], знали только служители, почерпавшие воду, – тогда распорядитель зовет жениха и говорит ему: всякий человек подает сперва хорошее вино, а когда напьются, тогда худшее; а ты хорошее вино сберег доселе. Так положил Иисус начало чудесам в Кане Галилейской и явил славу Свою; и уверовали в Него ученики Его.<o:p></o:p></span></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">(Ин 2:1–11)</div>"
+        }
+      ]
+    },
+    {
+      "id": "2-3-c-cha-gisu-rao-ging-nc-tri-v-ku-gi-sm-hi",
+      "name": "3. Возвещение Царствия Божия И Призыв К Обращению",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.05358848048561171,
+          "pitch": -0.17051613309329028,
+          "rotation": 0,
+          "target": "3-4-c-cha-gisu-bin-hnh-trn-ni"
+        },
+        {
+          "yaw": -1.7029965550555701,
+          "pitch": 0.1012106739874703,
+          "rotation": 0,
+          "target": "1-2-c-cha-gisu-d-tic-ci-cana"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.004657148241202336,
+          "pitch": -0.4721606572718109,
+          "title": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 115%;\">Возвещение Царствия Божия И Призыв К Обращению</span><o:p></o:p></p>",
+          "text": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">После же того, как предан был Иоанн, пришел Иисус в Галилею, проповедуя Евангелие Царствия Божия и говоря, что исполнилось время и приблизилось Царствие Божие: покайтесь и веруйте в Евангелие.<o:p></o:p></span></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">(Мк 1:14–15)</div>"
+        }
+      ]
+    },
+    {
+      "id": "3-4-c-cha-gisu-bin-hnh-trn-ni",
+      "name": "4. Преображение На Горе Фавор",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.7953274170598537,
+          "pitch": 0.37459110494686243,
+          "rotation": 0,
+          "target": "4-5-c-cha-gisu-lp-b-tch-thnh-th"
+        },
+        {
+          "yaw": -1.748073222986461,
+          "pitch": -0.012033693567310877,
+          "rotation": 0,
+          "target": "2-3-c-cha-gisu-rao-ging-nc-tri-v-ku-gi-sm-hi"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.71832037522912,
+          "pitch": -0.1197861019002886,
+          "title": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 115%;\">Преображение На Горе Фавор</span><o:p></o:p></p>",
+          "text":  "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">По прошествии дней шести, взял Иисус Петра, Иакова и Иоанна, брата его, и возвел их на гору высокую одних, и преобразился пред ними: и просияло лице Его, как солнце, одежды же Его сделались белыми, как свет. И вот, явились им Моисей и Илия, с Ним беседующие. При сем Петр сказал Иисусу: Господи! хорошо нам здесь быть; если хочешь, сделаем здесь три кущи: Тебе одну, и Моисею одну, и одну Илии. Когда он еще говорил, се, облако светлое осенило их; и се, глас из облака глаголющий: Сей есть Сын Мой Возлюбленный, в Котором Мое благоволение; Его слушайте. И, услышав, ученики пали на лица свои и очень испугались. Но Иисус, приступив, коснулся их и сказал: встаньте и не бойтесь. Возведя же очи свои, они никого не увидели, кроме одного Иисуса.<o:p></o:p></span></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">(Мф 17:1–8)</div>"
+        }
+      ]
+    },
+    {
+      "id": "4-5-c-cha-gisu-lp-b-tch-thnh-th",
+      "name": "5. Установление Евхаристии",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.7996773456601955,
+          "pitch": 0.0118117757142997,
+          "rotation": 0,
+          "target": "3-4-c-cha-gisu-bin-hnh-trn-ni"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.06865999545901147,
+          "pitch": -0.5026090379846924,
+          "title": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 115%;\">Установление Евхаристии</span><o:p></o:p></p>",
+          "text": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">И когда они ели, Иисус взял хлеб и, благословив, преломил и, раздавая ученикам, сказал: приимите, ядите: сие есть Тело Мое. И, взяв чашу и благодарив, подал им и сказал: пейте из нее все, ибо сие есть Кровь Моя Нового Завета, за многих изливаемая во оставление грехов.<o:p></o:p></span></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">(Мф 26:26–28)</div>"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
